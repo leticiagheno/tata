c21ecf3b0585d7b171ef3c4579b8af26ee2725bc
Letícia Gheno Baldissarelli <31359219+leticiagheno@users.noreply.github.com>
2022-02-09 01:19:36 -0300

    feat: criação dos componentes base para o site

---
+++ tata/src/components/Relogio.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1> {{ horario }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Relogio',
+  props: {
+    horario: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #FDBCD9;
+}
+</style>
---
+++ tata/src/components/Titulo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p :class="`${cor} ${tamanho}`"> {{ texto }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Titulo',
+  props: {
+    texto: String,
+    cor: String,
+    tamanho: String
+  }
+}
+</script>
+
+<style scoped>
+.verde {
+    color: #a8e077;
+}
+
+.rosa {
+    color: #FDBCD9;
+}
+
+.tiny {
+    font-size: 30px;
+}
+
+.normal {
+    font-size: 75px;
+}
+
+.big {
+    font-size: 150px;
+}
+
+</style>
