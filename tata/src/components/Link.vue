18423201263d50afe72dd1a4882283e7dded01a3
Letícia Gheno Baldissarelli <31359219+leticiagheno@users.noreply.github.com>
2022-02-10 23:34:35 -0300

    feat: criacao do componente link

---
+++ tata/src/components/Link.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <a :href="link" target="_blank"> By Let </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Link',
+  props: {
+      link: String
+  }
+}
+</script>
+
+<style scoped>
+
+a {
+    color: #FDBCD9;
+    font-size: 15px;
+}
+
+</style>
