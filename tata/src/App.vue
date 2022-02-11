a78faa1a21b50649b2274950e7bb694f31acb597
Letícia Gheno Baldissarelli <31359219+leticiagheno@users.noreply.github.com>
2022-02-10 23:36:14 -0300

    feat: alteradas nomenclaturas e adição de novos componentes

--- tata/src/App.vue
+++ tata/src/App.vue
@@ -1,22 +1,54 @@
 <template>
-  <div id="app">
-    <titulo texto="Faltam" cor="verde" tamanho="big"/>
-    <titulo texto="10h:50" cor="rosa"/>
-    <titulo texto="para a tata sair do trabalho" cor="verde"  tamanho="normal"/>
-    <titulo texto="By Let" cor="rosa" tamanho="tiny"/>
+  <div>
+    <div v-if="dentroHorarioTrabalho">
+      <app-titulo texto="Faltam" cor="verde" tamanho="big"/>
+      <app-relogio />
+      <app-titulo texto="para a tata sair do trabalho!" cor="verde"  tamanho="normal"/>
+    </div>
+    <div v-else> 
+        <app-titulo texto="Fora do horário de trabalho!" cor="verde" tamanho="big"/>
+    </div>
+    
+    <app-link class="padding-footer" link="https://github.com/leticiagheno"/>
   </div>
 </template>
 
 <script>
-import Titulo from './components/Titulo.vue';
+import AppTitulo from './components/Titulo.vue';
+import AppRelogio from './components/Relogio.vue';
+import AppLink from './components/Link.vue';
 
 export default {
   name: 'App',
   components: {
-    Titulo
+    AppTitulo,
+    AppRelogio,
+    AppLink
+  },
+  data() {
+    return {
+      dentroHorarioTrabalho: true
+    }
   }
 }
 </script>
-]
+
+
 <style>
+@import url('https://fonts.googleapis.com/css?family=Chelsea+Market');
+
+body {
+  font-family: 'Chelsea Market';
+  min-height: 100vh;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.padding-footer {
+  padding-top: 40px;
+  text-align: center;
+}
+
 </style>
